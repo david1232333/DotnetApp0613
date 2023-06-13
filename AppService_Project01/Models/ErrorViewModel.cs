02519cd5c1c8c4d4172d09aad586a1e3a3e1da68
davidshen <davidshen@DAVIDSHEN-NB2>
2023-06-13 15:46:14 +0800

    加入專案檔案。

---
+++ AppService_Project01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppService_Project01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
